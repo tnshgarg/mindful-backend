a46d81e67f5aebdefe392847fea0d9a2a691bb00
Tanish garg <tanishg566@outlook.com>
2023-08-17 20:31:17 +0530

    main commit

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=^0wwr6uhq%($lrdi#(d6z+xsum2dwd84b4ze6$ec(61+uwbyh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
